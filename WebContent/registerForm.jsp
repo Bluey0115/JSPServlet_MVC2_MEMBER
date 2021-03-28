e1f9b6f5a111a47e3b39c43b3ffb15065c6075db
bluey <helios1593@gmail.com>
2021-03-28 23:31:32 +0900

    회원가입 아이디 중복체크

--- WebContent/registerForm.jsp
+++ WebContent/registerForm.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>	
 <!DOCTYPE html>
 <html>
 <head>
@@ -65,6 +66,7 @@
 					<label for="inputUserName" class="sr-only">아이디</label> 
 					<input	type="text" class="form-control" placeholder="ID" name='userId' required autofocus>
 				</div>
+				<p>${error}
 				<div class="form-group">
 					<label for="inputPassword" class="sr-only">비밀번호</label> 
 					<input 	type="password" class="form-control" placeholder="Password" name='userPassword' required>
--- src/Controller/UserController.java
+++ src/Controller/UserController.java
@@ -35,168 +35,173 @@ public class UserController extends HttpServlet {
 		String RequestURI = request.getRequestURI();
 		String contextPath = request.getContextPath();
 		String command = RequestURI.substring(contextPath.length());
-		
+
 		response.setContentType("text/html; charset=utf-8");
 		request.setCharacterEncoding("utf-8");
-		
-		
-		// 회원가입 
-		if(command.equals("/register.do")) {
+
+		// 회원가입
+		if (command.equals("/register.do")) {
 			UserDAO userDAO = UserDAO.getinstance();
 			User user = new User();
+
+			String userId = request.getParameter("userId");
+
 			
-			user.setUserId(request.getParameter("userId"));
-			user.setUserPassword(request.getParameter("userPassword"));
-			user.setUserName(request.getParameter("userName"));
-			user.setUserEmail(request.getParameter("userEmail"));
+			int result = userDAO.confirmUserId(userId);
+			
+			if (result == 1) {
 				
-			userDAO.insertUser(user);
+				request.setAttribute("error", "해당 아이디가 이미 존재합니다");
+				RequestDispatcher rd = request.getRequestDispatcher("/registerForm.jsp");
+				rd.forward(request, response);
+			}
 			
+			
+			else if (result == -1) {
+
+				user.setUserId(request.getParameter("userId"));
+				user.setUserPassword(request.getParameter("userPassword"));
+				user.setUserName(request.getParameter("userName"));
+				user.setUserEmail(request.getParameter("userEmail"));
+				
+				userDAO.insertUser(user);
+				RequestDispatcher rd = request.getRequestDispatcher("/loginForm.jsp");
+				rd.forward(request, response);
+			}
+		}
+
+		// 로그인 설정
+		else if (command.equals("/login.do")) {
+
+			UserDAO userDAO = UserDAO.getinstance();
+			User user = new User();
+
+			String userId = request.getParameter("userId");
+			String userPassword = request.getParameter("userPassword");
+
+			int result = userDAO.userCheck(userId, userPassword);
+
+			if (result == 1) {
+
+				// 세션 등록
+				HttpSession session = request.getSession();
+				session.setAttribute("userId", userId);
+
+				RequestDispatcher rd = request.getRequestDispatcher("/index.jsp");
+				rd.forward(request, response);
+			} else if (result == 0) {
+
+				request.setAttribute("error", "비밀번호가 틀렸습니다.");
+				RequestDispatcher rd = request.getRequestDispatcher("/loginForm.jsp");
+				rd.forward(request, response);
+			} else if (result == -1)
+
+				request.setAttribute("error", "아이디가 존재하지 않습니다.");
 			RequestDispatcher rd = request.getRequestDispatcher("/loginForm.jsp");
 			rd.forward(request, response);
 		}
-		
-		
-		// 로그인 설정
-		else if(command.equals("/login.do")) {
-			
-			UserDAO userDAO = UserDAO.getinstance();
-			User user = new User();
-			
-			String userId = request.getParameter("userId");
-			String userPassword = request.getParameter("userPassword");
-			
-			
-			int result = userDAO.userCheck(userId, userPassword);
-			
-			if(result == 1) {
-			
-			// 세션 등록	
-			HttpSession session  = request.getSession();
-			session.setAttribute("userId", userId);
-				
-			RequestDispatcher rd = request.getRequestDispatcher("/index.jsp");
-			rd.forward(request, response);
-			}
-			else if(result == 0) {
-	
-				request.setAttribute("error","비밀번호가 틀렸습니다.");
-				RequestDispatcher rd = request.getRequestDispatcher("/loginForm.jsp");
-				rd.forward(request, response);
-			}
-			else if(result == -1)
-				
-				request.setAttribute("error", "아이디가 존재하지 않습니다.");
-				RequestDispatcher rd = request.getRequestDispatcher("/loginForm.jsp");
-				rd.forward(request, response);
-		}
-		
-		//로그 아웃
-		else if(command.equals("/logout.do")) {
-			
-			HttpSession session  = request.getSession();
+
+		// 로그 아웃
+		else if (command.equals("/logout.do")) {
+
+			HttpSession session = request.getSession();
 			session.invalidate();
-			
+
 			RequestDispatcher rd = request.getRequestDispatcher("/index.jsp");
 			rd.forward(request, response);
-			
+
 		}
-		
-		//정보 수정 확인 페이지
-		else if(command.equals("/upChceckForm.do")) {
-			
+
+		// 정보 수정 확인 페이지
+		else if (command.equals("/upChceckForm.do")) {
+
 			UserDAO userDAO = UserDAO.getinstance();
 			User user = new User();
-			
+
 			String userId = request.getParameter("userId");
 			String userPassword = request.getParameter("userPassword");
-			
+
 			int result = userDAO.userCheck(userId, userPassword);
-			
-			if(result == 1) {
-				
-			user = userDAO.getUser(userId,userPassword);
-			
-			request.setAttribute("u", user);
-			
-			RequestDispatcher rd = request.getRequestDispatcher("/updateForm.jsp");
-			rd.forward(request, response);
-			}
-			else if(result == 0) {
-	
-				request.setAttribute("error","비밀번호가 틀렸습니다.");
+
+			if (result == 1) {
+
+				user = userDAO.getUser(userId, userPassword);
+
+				request.setAttribute("u", user);
+
+				RequestDispatcher rd = request.getRequestDispatcher("/updateForm.jsp");
+				rd.forward(request, response);
+			} else if (result == 0) {
+
+				request.setAttribute("error", "비밀번호가 틀렸습니다.");
 				RequestDispatcher rd = request.getRequestDispatcher("/upCheckForm.jsp");
 				rd.forward(request, response);
-			}
-			else if(result == -1) {
-				
+			} else if (result == -1) {
+
 				request.setAttribute("error", "아이디가 존재하지 않습니다.");
 				RequestDispatcher rd = request.getRequestDispatcher("/upCheckForm.jsp");
 				rd.forward(request, response);
 			}
 		}
-		
-		
+
 		// 회원 정보 수정 처리
-		else if(command.equals("/update.do")) {
-			
+		else if (command.equals("/update.do")) {
+
 			UserDAO userDAO = UserDAO.getinstance();
 			User user = new User();
-			
+
 			user.setUserId(request.getParameter("userId"));
 			user.setUserPassword(request.getParameter("userPassword"));
 			user.setUserName(request.getParameter("userName"));
 			user.setUserEmail(request.getParameter("userEmail"));
-			
+
 			System.out.println(user);
-			
+
 			int result = userDAO.updateUser(user);
-			
-			
-			if(result == 1) {
-				
-			request.setAttribute("message","회원정보가 수정이 완료되었습니다.");
-			RequestDispatcher rd = request.getRequestDispatcher("/index.jsp");
-			rd.forward(request, response);
-			
+
+			if (result == 1) {
+
+				request.setAttribute("message", "회원정보가 수정이 완료되었습니다.");
+				RequestDispatcher rd = request.getRequestDispatcher("/index.jsp");
+				rd.forward(request, response);
+
 			}
-			
-			else if(result == 0) {
-				
-			request.setAttribute("message", "회원정보 수정에 실패 하셨습니다.");
-			RequestDispatcher rd = request.getRequestDispatcher("/index.jsp");
-			rd.forward(request, response);
-			
-			}			
+
+			else if (result == 0) {
+
+				request.setAttribute("message", "회원정보 수정에 실패 하셨습니다.");
+				RequestDispatcher rd = request.getRequestDispatcher("/index.jsp");
+				rd.forward(request, response);
+
+			}
 		}
-		
+
 		// 회원 탈퇴
-		else if(command.equals("/delete.do")) {
-		
+		else if (command.equals("/delete.do")) {
+
 			UserDAO userDAO = UserDAO.getinstance();
 			String userId = request.getParameter("userId");
 			String userPassword = request.getParameter("userPassword");
-			
+
 			System.out.println(userId + userPassword);
-			
-			int result = userDAO.deleteUser(userId,userPassword);
-			
-			if(result == 1) {
-			
-			HttpSession session  = request.getSession();
-			
-			System.out.println(session);
-			
-			session.invalidate();
-			RequestDispatcher rd = request.getRequestDispatcher("/index.jsp");
-			rd.forward(request, response);
-			
-			}
-			else if(result == 0) {
-			
-			request.setAttribute("error", "비밀번호가 맞지 않습니다.");
-			RequestDispatcher rd = request.getRequestDispatcher("/upCheckForm.jsp");
-			rd.forward(request, response);
+
+			int result = userDAO.deleteUser(userId, userPassword);
+
+			if (result == 1) {
+
+				HttpSession session = request.getSession();
+
+				System.out.println(session);
+
+				session.invalidate();
+				RequestDispatcher rd = request.getRequestDispatcher("/index.jsp");
+				rd.forward(request, response);
+
+			} else if (result == 0) {
+
+				request.setAttribute("error", "비밀번호가 맞지 않습니다.");
+				RequestDispatcher rd = request.getRequestDispatcher("/upCheckForm.jsp");
+				rd.forward(request, response);
 			}
 		}
 	}
